32f212977d5c2cbd7daaeb1489f3ee72b0b0412d
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2026-07-02 00:39:08 +0100

    delete musics

--- src/lib/music.ts
+++ src/lib/music.ts
@@ -5,12 +5,11 @@ export interface Track {
 }
 
 export const TRACKS: Track[] = [
-  { id: 1, title: "Vice City Dream 1", src: "/audio/track-01.mp3" },
-  { id: 2, title: "Vice City Dream 2", src: "/audio/track-02.mp3" },
-  
-  { id: 3, title: "Love Is A Long Road", src: "/audio/Love_Is_A_Long_Road.mp3" },
-  { id: 4, title: "Love Is A Long Road 2", src: "/audio/Love_Is_A_Long_Road2.mp3" },
-  { id: 5, title: "Miami Vibe", src: "/audio/Miami_Vibe.mp3" },
+  { id: 1, title: "Vice City Dream 1", src: "https://github.com/aabderahman190-oss/gtimer_video/releases/download/v2.0/track-01.mp3" },
+  { id: 2, title: "Vice City Dream 2", src: "https://github.com/aabderahman190-oss/gtimer_video/releases/download/v2.0/track-02.mp3" },
+  { id: 3, title: "Love Is A Long Road", src: "https://github.com/aabderahman190-oss/gtimer_video/releases/download/v2.0/Love_Is_A_Long_Road.mp3" },
+  { id: 4, title: "Love Is A Long Road 2", src: "https://github.com/aabderahman190-oss/gtimer_video/releases/download/v2.0/Love_Is_A_Long_Road2.mp3" },
+  { id: 5, title: "Miami Vibe", src: "https://github.com/aabderahman190-oss/gtimer_video/releases/download/v2.0/Miami_Vibe.mp3" },
 
 
 ];
