a50652bc8d3224c472bab11b7fc05641a3abc723
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2026-07-03 16:05:29 +0100

    change /og-image.svg to /og-image.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,13 +15,13 @@ export const metadata: Metadata = {
     siteName: SITE_NAME,
     locale: "en_US",
     type: "website",
-    images: [{ url: "/og-image.svg", width: 1200, height: 630 }],
+    images: [{ url: "/og-image.png", width: 1200, height: 630 }],
   },
   twitter: {
     card: "summary_large_image",
     title: `${SITE_NAME} - GTA VI Countdown`,
     description: SITE_DESCRIPTION,
-    images: ["/og-image.svg"],
+    images: ["/og-image.png"],
   },
   robots: {
     index: true,
